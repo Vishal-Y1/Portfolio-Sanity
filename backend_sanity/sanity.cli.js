e46d36573c6b57a8d1b0ec045e136e5393b2fdc3
Vishal Yadav <merndevvishal@gmail.com>
2024-02-20 12:38:43 +0530

    Portfolio main First commit

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hpj17wqu',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,11 @@
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import contact from './contact'
+import skills from './skills'
+import experiences from './experiences'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [testimonials, brands, abouts, contact, skills, experiences, workExperience, works]
+
